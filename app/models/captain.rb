01872569f13fe14a787593b72545442b88ce348b
hwalborn <hwalborn@gmail.com>
2017-02-17 07:03:26 -0500

    Gonna need a bigger boat

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,19 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    joins(boats: :classifications).where(classifications: {name: "Catamaran"})
+  end
+
+  def self.sailors
+    joins(boats: :classifications).where(classifications: {name: "Sailboat"}).uniq
+  end
+
+  def self.talented_seamen
+    joins(boats: :classifications).where(classifications: {name: "Motorboat"}, boats: {captain_id: sailors.pluck(:id)})
+  end
+
+  def self.non_sailors
+    all.where.not(captains: {name: sailors.pluck(:name)})
+  end
 end
